e6153ea9370bd4e9669d48eae4b3691f28b1bae0
刘裕 <liuyu@healthmall.cn>
2017-11-17 17:02:11 +0800

    使用karma+jasmine测试，并且使用webpack

--- src/array/arrayEqual.js
+++ src/array/arrayEqual.js
@@ -5,13 +5,11 @@
  * @param {Array} arr2
  * @return {Boolean}
  */
-
-function arrayEqual(arr1, arr2) {
+module.exports = (arr1, arr2) => {
 	if (arr1 === arr2) return true;
 	if (arr1.length != arr2.length) return false;
-	for (let i = 0; i < arr1.length; ++i) {
+	for (var i = 0; i < arr1.length; ++i) {
 		if (arr1[i] !== arr2[i]) return false;
 	}
 	return true;
 }
-//module.exports = arrayEqual;
